debd1db6368109fa50e3eb0bceca6d3089ca76a5
guoxiwei <1451364321@qq.com>
2019-08-02 20:16:10 +0800

    sdsds

--- src/main/java/com/otheima/git/User.java
+++ src/main/java/com/otheima/git/User.java
@@ -2,7 +2,7 @@ package com.otheima.git;
 
 public class User {
     private String name;//用户名
-    private Integer age;
+    private Integer age;//年龄
 
     public String getName() {
         return name;
