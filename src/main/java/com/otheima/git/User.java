52e6c3591ae842d680d64e29c6f4e0677520bc4b
guoxiwei <1451364321@qq.com>
2019-08-02 21:09:29 +0800

    asasa

--- src/main/java/com/otheima/git/User.java
+++ src/main/java/com/otheima/git/User.java
@@ -20,4 +20,12 @@ public class User {
     public void setAge(Integer age) {
         this.age = age;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
